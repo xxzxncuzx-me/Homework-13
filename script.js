6715f2967d4f28db51f951db0bd2d93d65f436d5
Iryna Fedorova <i.fedorova4321@gmailcom>
2023-01-04 13:49:57 +0200

    Homework 13

---
+++ script.js
@@ -0,0 +1,27 @@
+// Отримати від користувача 3 рядки та вивести їх у довільному порядку однією командою (конкатенація);
+
+const strFirst = prompt('Введіть перший рядок');
+const strSecond = prompt('Введіть другий рядок');
+const strThird = prompt('Введіть третій рядок');
+
+if (!strFirst || !strSecond || !strThird) {
+  alert('Ви нічого не ввели!');
+} else {
+  alert(`${strThird}, ${strFirst}, ${strSecond}.`);
+}
+
+// Розбити за цифрами п'ятизначне число і вивести у вихідному порядку через пробіл.
+
+const number = prompt('Введіть п\'ятизначне число');
+
+if (!number) {
+  alert('Ви нічого не ввели!');
+} else if (isNaN(number) || number.length !== 5) {
+  alert('Ви ввели невалідні дані!');
+} else {
+  function splitNumber(num) {
+    return num.split('').join(' ');
+  }
+}
+const array = splitNumber(number);
+alert(array);
